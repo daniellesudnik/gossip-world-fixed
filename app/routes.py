7538485e91d7a94d3bda23786893ff7d456430a7
daniellesudnik <dasudn@icloud.com>
2026-04-02 18:24:50 +0200

    fix: remove read_only from app - causes startup issues

--- app/routes.py
+++ app/routes.py
@@ -229,4 +229,4 @@ if __name__ == '__main__':
     database = DataBase(dbEndpoint, dbUser, dbPassword, dbName)
     init_db(database)
 
-    app.run(host='127.0.0.1', port=10007, debug=False)
+    app.run(host='0.0.0.0', port=10007, debug=False)
